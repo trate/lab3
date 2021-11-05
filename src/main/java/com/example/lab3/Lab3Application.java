7ee9e95c75254d5a66a700db0559e2ce230b6f31
Oleg Laktionov <ohlegik@gmail.com>
2021-11-05 17:16:34 +0300

    Fix pom.xml

--- src/main/java/com/example/lab3/Lab3Application.java
+++ src/main/java/com/example/lab3/Lab3Application.java
@@ -1,13 +1,23 @@
 package com.example.lab3;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab3Application {
 
+
+	@RequestMapping("/")
+		String home() {
+				return "Hello World!";
+		}
+
 	public static void main(String[] args) {
-		SpringApplication.run(Lab3Application.class, args);
-	}
+				SpringApplication.run(Lab3Application.class, args);
+									}
 
 }
+
